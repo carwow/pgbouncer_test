0b8ca4a3278ff04a1e2aff5d32180fe6a73b36c7
Christian Gregg <christian@bissy.io>
2021-01-23 18:52:55 +0000

    Add request.queue_time_ms to honeycomb
    Signed-off-by: Christian Gregg <christian@bissy.io>

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -1,4 +1,6 @@
 class PingController < ApplicationController
+  before_action :add_queue_time
+
   def handle
     db = rand * 2
     cpu = rand
@@ -6,5 +8,16 @@ class PingController < ApplicationController
     sleep cpu
 
     render json: { ok: true }
+  end
+
+  private
+
+  def add_queue_time
+    now = (Time.current.to_f * 1000).to_i
+    start = request.headers["HTTP_X_REQUEST_START"].to_i
+
+    queue_time_ms = now - start
+
+    Honeycomb.add_field('request.queue_time_ms', queue_time_ms)
   end
 end
