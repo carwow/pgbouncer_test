d4ff46f91b28a03e1aa790fc867b8319b62d986f
Christian Gregg <christian@bissy.io>
2021-01-23 18:59:13 +0000

    Add pbbouncer?
    Signed-off-by: Christian Gregg <christian@bissy.io>

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -1,28 +1,37 @@
 class PingController < ApplicationController
-  before_action :add_queue_time
-  before_action :add_test_name
-
   def handle
-    db = rand * 2
-    cpu = rand
-    ApplicationRecord.connection.execute("SELECT pg_sleep(#{db})")
-    sleep cpu
+    Honeycomb.add_field('queue_time_ms', queue_time_ms)
+    Honeycomb.add_field('test_name', test_name)
+    Honeycomb.add_field('pgbouncer?', pgbouncer?)
+
+    sleep_db
+    sleep_cpu
 
     render json: { ok: true }
   end
 
   private
 
-  def add_queue_time
+  def sleep_cpu
+    sleep rand
+  end
+
+  def sleep_db
+    ApplicationRecord.connection.execute("SELECT pg_sleep(#{rand * 2})")
+  end
+
+  def queue_time_ms
     now = (Time.current.to_f * 1000).to_i
     start = request.headers["HTTP_X_REQUEST_START"].to_i
 
-    queue_time_ms = now - start
-
-    Honeycomb.add_field('queue_time_ms', queue_time_ms)
+    now - start
   end
 
-  def add_test_name
-    Honeycomb.add_field('test_name', params[:test_name])
+  def pgbouncer?
+    ENV.fetch("PGBOUNCER_ENABLED", "false") == "true"
+  end
+
+  def test_name
+    params[:test_name]
   end
 end
