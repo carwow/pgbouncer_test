11fb984eb39494c5b1a96d0d04d38eb24e8434d2
Christian Gregg <christian@bissy.io>
2021-01-24 13:21:58 +0000

    Make sleep bounds configurable
    Signed-off-by: Christian Gregg <christian@bissy.io>

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -1,23 +1,28 @@
 class PingController < ApplicationController
-  def handle
-    Honeycomb.add_field('queue_time_ms', queue_time_ms)
-    Honeycomb.add_field('test_name', test_name)
-    Honeycomb.add_field('pgbouncer?', pgbouncer?)
+  before_action :honey
 
-    sleep_db
-    sleep_cpu
+  def handle
+    cpu_sleep = params[:cpu].to_i / 1000.0
+    db_sleep = params[:db].to_i / 1000.0
+
+    ApplicationRecord.connection.execute("SELECT pg_sleep(#{db_sleep})")
+    sleep cpu_sleep
 
     render json: { ok: true }
   end
 
   private
 
-  def sleep_cpu
-    sleep rand
+  def honey
+    Honeycomb.add_field('queue_time_ms', queue_time_ms)
+    Honeycomb.add_field('pgbouncer?', pgbouncer?)
+    Honeycomb.add_field('test_name', params[:test_name])
+    Honeycomb.add_field('sleep_cpu', params[:cpu])
+    Honeycomb.add_field('sleep_db', params[:db])
   end
 
-  def sleep_db
-    ApplicationRecord.connection.execute("SELECT pg_sleep(#{rand * 2})")
+  def pgbouncer?
+    ENV.fetch("PGBOUNCER_ENABLED", "false") == "true"
   end
 
   def queue_time_ms
@@ -25,13 +30,5 @@ class PingController < ApplicationController
     start = request.headers["HTTP_X_REQUEST_START"].to_i
 
     now - start
-  end
-
-  def pgbouncer?
-    ENV.fetch("PGBOUNCER_ENABLED", "false") == "true"
-  end
-
-  def test_name
-    params[:test_name]
   end
 end
